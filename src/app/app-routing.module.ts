c2f56f57dde7f9e517330e0e31c89c56834d9018
minhducdma@gmail.com <minhducdma@gmail.com>
2021-05-23 20:53:00 +0700

    update ROUTE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,45 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NoAuthGuard } from './core/guard/no-auth.guard';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/auth/login',
+        pathMatch: 'full'
+    },
+    {
+        path: '',
+        component: ContentLayoutComponent,
+        canActivate: [NoAuthGuard], // Should be replaced with actual auth guard
+        children: [
+            {
+                path: 'dashboard',
+                loadChildren: () =>
+                    import('./modules/home/home.module').then(m => m.HomeModule)
+            }
+        ]
+    },
+    {
+        path: 'auth',
+        component: AuthLayoutComponent,
+        loadChildren: () =>
+            import('./modules/auth/auth.module').then(m => m.AuthModule)
+    },
+    // Fallback when no prior routes is matched
+    { path: '**', redirectTo: '/auth/login', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, {
+            useHash: true,
+            relativeLinkResolution: 'legacy'
+        })
+    ],
+    exports: [RouterModule],
+    providers: []
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarLayoutComponent } from './layout/navbar-layout/navbar-layout.component';
+import { FooterLayoutComponent } from './layout/footer-layout/footer-layout.component';
+import { AuthModule } from './modules/auth/auth.module';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentLayoutComponent,
+    NavbarLayoutComponent,
+    FooterLayoutComponent,
+    AuthLayoutComponent
   ],
   imports: [
+    // angular
     BrowserModule,
-    AppRoutingModule
+
+    // 3rd party
+    AuthModule,
+
+    // core & shared
+    CoreModule,
+    SharedModule,
+
+    // app
+    AppRoutingModule,
+
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
