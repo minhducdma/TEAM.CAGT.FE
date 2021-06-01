c4780c594431a222480b571eb3a1759ed90c4974
Your Name <you@example.com>
2021-06-01 15:50:41 +0700

    fe themes qit

--- src/app/layout/content-layout/content-layout.component.ts
+++ src/app/layout/content-layout/content-layout.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-content-layout',
   templateUrl: './content-layout.component.html',
+  styleUrls: ['./content-layout.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ContentLayoutComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
 }
